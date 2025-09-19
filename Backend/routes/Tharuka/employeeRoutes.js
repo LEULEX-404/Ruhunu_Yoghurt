4b868b74b2961000ec902882ba3119e7239d1c5d
Miyuru Tharuka <miyurut20@gmail.com>
2025-09-19 17:51:26 +0530

    Search for Assigned Employees

--- Backend/routes/Tharuka/employeeRoutes.js
+++ Backend/routes/Tharuka/employeeRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createEmployee, getEmployees, updateEmployee, deleteEmployee } from "../../controllers/Tharuka/employeeController.js";
+import { createEmployee, getEmployees, updateEmployee, deleteEmployee, getSearchEmployee } from "../../controllers/Tharuka/employeeController.js";
 
 const router= express.Router();
 
@@ -7,5 +7,6 @@ router.post("/add",createEmployee);
 router.get("/",getEmployees);
 router.put("/update/:id",updateEmployee);
 router.delete("/delete/:id",deleteEmployee);
+router.get("/search",getSearchEmployee);
 
 export default router;

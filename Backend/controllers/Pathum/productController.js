f8b4ac0c2757b08d1ffd25f6a5154124ed952910
Pathum Bandara <pathumbandara1234@gmail.com>
2025-09-20 20:36:29 +0530

    Implement update product (Backend)

--- Backend/controllers/Pathum/productController.js
+++ Backend/controllers/Pathum/productController.js
@@ -40,3 +40,31 @@ export async function getProduct(req, res){
         })
     }
 }
+
+export async function updateProduct(req, res){
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message : "You are not authorized to update a product"
+        })
+        return
+    }
+
+    const productId = req.params.productId
+    const updatingData = req.body
+
+    try {
+        await Product.updateOne(
+            {productId : productId},
+            updatingData
+        )
+
+        res.json({
+            message : "Product updated successfully"
+        })
+    } catch (err) {
+        res.status(500).json({
+            message : "Internal server error",
+            error : err
+        })
+    }
+}
--- Backend/routes/Pathum/productRoute.js
+++ Backend/routes/Pathum/productRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getProduct, saveProduct } from '../../controllers/Pathum/productController.js';
+import { getProduct, saveProduct, updateProduct } from '../../controllers/Pathum/productController.js';
 
 const productRouter = express.Router();
 
-productRouter.post("/", saveProduct)
+productRouter.post("/", saveProduct);
 productRouter.get("/", getProduct);
+productRouter.get("/:productId", updateProduct)
 
 export default productRouter;

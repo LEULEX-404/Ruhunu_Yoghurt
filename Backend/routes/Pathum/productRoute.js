e0958b1b522cd32e30ec5959b2582bb15917f020
Pathum Bandara <pathumbandara1234@gmail.com>
2025-09-25 22:22:38 +0530

    admin function issue fixed

--- Backend/routes/Pathum/productRoute.js
+++ Backend/routes/Pathum/productRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { addRating, deleteProduct, getProduct, getProductById, saveProduct, searchProducts, updateProduct } from '../../controllers/Pathum/productController.js';
+import { authMiddleware } from '../../middleware/auth.js';
+
 
 const productRouter = express.Router();
 
-productRouter.post("/", saveProduct);
+productRouter.post("/", authMiddleware, saveProduct);
 productRouter.get("/", getProduct);
-productRouter.put("/:productId", updateProduct)
-productRouter.delete("/:productId", deleteProduct)
+productRouter.put("/:productId", authMiddleware, updateProduct)
+productRouter.delete("/:productId", authMiddleware, deleteProduct)
 productRouter.post("/add-rating", addRating)
 productRouter.get("/search/:query", searchProducts)
 productRouter.get("/:productId", getProductById)

406e467aeb7208be775b9cc5c06561480383af05
Kalindu123-web <kalindumethmuditha03@gmail.com>
2025-09-29 14:10:20 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import { Toaster } from "react-hot-toast";
 
@@ -14,15 +14,11 @@ import HomePage from './Pages/HomePage';
 import OrderDashboard from './Pages/OrderDashboard';
 import CartPreview from './Components/CartPreview';
 import ProductPage from './Pages/customer/productsPage';
-import ProductOverViewPage from './Pages/customer/productOverview'
+import ProductOverViewPage from './Pages/customer/productOverview';
 import CartPage from './Pages/customer/cart';
-import { Toaster } from 'react-hot-toast';
-
-//import PaymentPage from './Pages/customer/payment';
 import PaymentPage from './Pages/PaymentPage';
 import AdminPage from './Pages/adminPage';
 import SearchProductPage from './Pages/customer/searchProduct';
-
 
 import AddSupplierPage from './Pages/admin/addSupplier';
 import AddRawMaterialPage from './Pages/admin/addRawmaterial';
@@ -37,63 +33,13 @@ import AddRemoveProductForm from './Pages/admin/add productstock';
 import Smdashboardpage from './Pages/admin/smdashboard';
 import ProductTable from './Pages/StockDashboard';
 
-
 axios.defaults.baseURL = 'http://localhost:8070'; 
 axios.defaults.withCredentials = true;
 
 function Appwrapper() {
-
-  return(
+  return (
     <div>
-
-       <Toaster position='top-right'/>
-    <Routes>
-
-
-      <Route path="/" element={<Navigate to="/home" replace/>} />
-
-
-    <Routes>
-
-      <Route path="/" element={<Navigate to="/login" replace/>} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/userProfile" element={<UserProfile/>}/>
-      <Route path="/hrDashboard" element={<HrDashboard />} />
-      <Route path="/deliveryDashboard" element ={<DeliveryDashboard/>} />
-      <Route path="/attendence" element ={<AttendencePage/>} />
-      <Route path="/driverPortal" element ={<DriverPortal/>} />
-      <Route path="/orderDashboard" element ={<OrderDashboard/>} />
-      <Route path="/cart/preview" element ={<CartPreview/>} />
-
-      <Route path='/products' element = {<ProductPage/>}/>
-      <Route path='/overview/:id' element={<ProductOverViewPage/>}/>
-      <Route path='/cart' element={<CartPage/>}/>
-      <Route path='/cart/preview' element={<CartPage/>}/>
-      <Route path='/payment' element={<PaymentPage/>}/>
-
-      <Route path='/search' element={<SearchProductPage/>}/>
-      <Route path='/admin/*' element={<AdminPage/>}/>
-    </Routes>
-      <Route path="/orderDashboard" element={<OrderDashboard />} />
-      <Route path="/addSupplier" element={<AddSupplierPage/>} />
-      <Route path="/addRawmaterial" element={<AddRawMaterialPage/>}/>
-      <Route path="/updateSupplier/:id" element={<UpdateSupplierPage />} />
-      <Route path="/updateRawmaterial/:id" element={<UpdateRawMaterialPage/>}/>
-      <Route path="/suplierTable" element={<SupplierTable/>}/>
-      <Route path="/rawmaterialTable" element={<RawMaterialTable/>}/>
-      <Route path="/stockpage" element={<Stockpage/>}/>
-      <Route path="/addrawmaterialform" element={<AddRawMaterialForm/>}/>
-      <Route path="/rawmaterialhistory" element={<RawMaterialWithHistory/>}/>
-      <Route path="/addproductform" element={<AddRemoveProductForm/>}/>
-      <Route path="/smdashboard" element={<Smdashboardpage/>}/>
-      <Route path="/productStock" element={<ProductTable/>}/>
-    
-      
-
-
-    </Routes>
-    <Toaster 
+      <Toaster 
         position="top-right"
         toastOptions={{
           style: {
@@ -103,17 +49,57 @@ function Appwrapper() {
           },
         }}
       />
+
+      <Routes>
+        {/* Default redirects */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/home" element={<HomePage />} />
+
+        {/* Auth */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* User */}
+        <Route path="/userProfile" element={<UserProfile />} />
+        <Route path="/attendence" element={<AttendencePage />} />
+
+        {/* Manager/Dashboard */}
+        <Route path="/hrDashboard" element={<HrDashboard />} />
+        <Route path="/deliveryDashboard" element={<DeliveryDashboard />} />
+        <Route path="/orderDashboard" element={<OrderDashboard />} />
+        <Route path="/driverPortal" element={<DriverPortal />} />
+        <Route path="/smdashboard" element={<Smdashboardpage />} />
+        <Route path="/stockpage" element={<Stockpage />} />
+        <Route path="/productStock" element={<ProductTable />} />
+
+        {/* Admin */}
+        <Route path="/admin/*" element={<AdminPage />} />
+        <Route path="/addSupplier" element={<AddSupplierPage />} />
+        <Route path="/addRawmaterial" element={<AddRawMaterialPage />} />
+        <Route path="/updateSupplier/:id" element={<UpdateSupplierPage />} />
+        <Route path="/updateRawmaterial/:id" element={<UpdateRawMaterialPage />} />
+        <Route path="/suplierTable" element={<SupplierTable />} />
+        <Route path="/addrawmaterialform" element={<AddRawMaterialForm />} />
+        <Route path="/rawmaterialhistory" element={<RawMaterialWithHistory />} />
+        <Route path="/addproductform" element={<AddRemoveProductForm />} />
+
+        {/* Customer */}
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/overview/:id" element={<ProductOverViewPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/cart/preview" element={<CartPreview />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/search" element={<SearchProductPage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-function App(){
-  return(
+function App() {
+  return (
     <Router>
       <Appwrapper />
     </Router>
-  )
+  );
 }
 
 export default App;
-

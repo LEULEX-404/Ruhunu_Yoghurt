98776e9bb5779cbb8734b4083181b10beec87cb0
Miyuru Tharuka <miyurut20@gmail.com>
2026-02-04 17:39:48 +0530

    Update HrDashboard.jsx

--- frontend/src/Pages/HrDashboard.jsx
+++ frontend/src/Pages/HrDashboard.jsx
@@ -396,10 +396,7 @@ export default function HrDashboard() {
                   >
                     <FiHome /> Dashboard
                   </li>
-                  <li 
-                    className={view === 'add' ? 'active' : ''} 
-                    onClick={() => setView('add')}
-                  >
+                  
                    <FiUserPlus /> Add Employee
                   </li>
                   <li 
